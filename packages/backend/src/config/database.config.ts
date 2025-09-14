f4b603588ba31d3043255fc21c28b98554dc3250
Divine <divief837@gmail.com>
2025-09-14 16:12:47 +0100

    changed db URL to URI

--- packages/backend/src/config/database.config.ts
+++ packages/backend/src/config/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export const databaseConfig = registerAs('database', () => ({
-  url: process.env.DATABASE_URL,
+  url: process.env.DATABASE_URI,
   ssl: process.env.NODE_ENV === 'production',
   logging: process.env.NODE_ENV === 'development',
 }));
